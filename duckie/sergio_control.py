TODO: bug in jax gradient poisson

duckie/sergio_control.py
@@ -902,12 +902,15 @@ class sergio(object):
         scData_log = jnp.log(jnp.add(scData, 1))
         log_mid_point = jnp.percentile(scData_log, percentile)
         prob_ber = jnp.true_divide(1, 1 + jnp.exp(-1 * shape * (scData_log - log_mid_point)))
-        binary_ind = jnp.array(onp.random.binomial(n=1, p=onp.array(jax.lax.stop_gradient(prob_ber))))
+        # binary_ind = jnp.array(onp.random.binomial(n=1, p=onp.array(jax.lax.stop_gradient(prob_ber))))
+        key = jax.random.PRNGKey(0)
+        binary_ind = jax.random.bernoulli(key, p=prob_ber)
+        # binary_ind = jnp.array(jnp.random.binomial(n=1, p=prob_ber))
         return binary_ind
 
     def convert_to_UMIcounts(self, scData):
         """ Input: scData can be the output of simulator or any refined version of it (e.g. with technical noise) """
-        return jnp.random.poisson(jax.lax.stop_gradient(scData))
+        return jnp.random.poisson(scData)
 
     """""""""""""""""""""""""""""""""""""""""""""""""""""""""
     "" This part is to add technical noise to dynamics data

duckie/src/envs.py
@@ -36,11 +36,11 @@ class EnvControlSteadyState(object):
         expr_add_outlier_genes = self.env.outlier_effect(expression, outlier_prob=0.01, mean=0.8, scale=1)
         _, expression_with_outliers = self.env.lib_size_effect(expr_add_outlier_genes, mean=4.6, scale=0.4)
         binary_dropout_indices = self.env.dropout_indicator(expression_with_outliers, shape=6.5, percentile=82)
-        # expression_with_outliers_and_dropout = jnp.multiply(binary_dropout_indices, expression_with_outliers)
-        # count_matrix_umi_count_format = self.env.convert_to_UMIcounts(expression_with_outliers_and_dropout)
-        # count_expression_matrix = jnp.concatenate(count_matrix_umi_count_format, axis=1)
-        # transpose_count_matrix = count_expression_matrix.T
-        return binary_dropout_indices
+        expression_with_outliers_and_dropout = jnp.multiply(binary_dropout_indices, expression_with_outliers)
+        count_matrix_umi_count_format = self.env.convert_to_UMIcounts(expression_with_outliers_and_dropout)
+        count_expression_matrix = jnp.concatenate(count_matrix_umi_count_format, axis=1)
+        transpose_count_matrix = count_expression_matrix.T
+        # return binary_dropout_indices
         return transpose_count_matrix
 
 