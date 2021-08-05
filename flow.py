e0c978da04e74b6577a563d22924eab47b319c8c
ioneliabuzatu <buzatuionelia@gmail.com>
2021-08-05 21:39:16 +0200

    break cycles in grn

--- flow.py
+++ flow.py
@@ -3,10 +3,10 @@ import config
 from scenicsergio.scenic_sergio_flow import ScenicSergioFlow
 
 flow = ScenicSergioFlow(
-    filepath_control_adjancies=config.filepath_adjancies_control,
-    filepath_disease_adjancies=config.filepath_adjancies_disease,
+    filepath_control_adjacency=config.filepath_adjancies_control,
+    filepath_disease_adjacency=config.filepath_adjancies_disease,
     filepath_to_save_interactions="scenicsergio/both_interactions.txt",
     filepath_to_save_regulons="scenicsergio/both_regulons.txt",
-    select_percent_adjancies=2
+    select_percentile_adjacency=0.002
 )
-flow.make_grn_sergio()
+flow.make_grn_sergio(make_one_state_only=True, make_which_state="control")
--- scenicsergio/scenic_sergio_flow.py
+++ scenicsergio/scenic_sergio_flow.py
@@ -1,120 +1,171 @@
-import config
 import json
-import tqdm
-import pandas as pd
 import logging
+
+import matplotlib.pyplot as plt
+import networkx as nx
 import numpy as np
 
 
 class ScenicSergioFlow(object):
     def __init__(self,
-                 filepath_control_adjancies: str,
-                 filepath_disease_adjancies: str,
                  filepath_to_save_interactions: str,
                  filepath_to_save_regulons: str,
-                 select_percent_adjancies: int = 100
+                 select_percentile_adjacency=100,
+                 filepath_control_adjacency: str = None,
+                 filepath_disease_adjacency: str = None,
+                 filepath_save_gene_name_to_id_mapping: str = "/tmp/gene_names_mapping_to_ids_in_grn.json",
                  ):
         """
         Output Adjacencies from scenic comes as: columns are in order of {TF \tab Target \tab Importance}
-        select_percent_adjancies: the portion of the original matrix to work with
+        select_percent_adjacency: the portion of the original matrix to work with
         """
-        self.control_filepath = filepath_control_adjancies
-        self.disease_filepath = filepath_disease_adjancies
+        self.control_filepath = filepath_control_adjacency
+        self.disease_filepath = filepath_disease_adjacency
         self.filepath_to_save_interactions = filepath_to_save_interactions
         self.filepath_to_save_regulons = filepath_to_save_regulons
-        self.select_percent_adjancies = select_percent_adjancies
-        self.control_adjancies, self.disease_adjancies = self.open_adjancies
+        self.select_percent_adjacency = select_percentile_adjacency
+        self.filepath_save_gene_name_to_id_mapping = filepath_save_gene_name_to_id_mapping
+        self.control_adjacency = self.disease_adjacency = None
 
-    @property
-    def open_adjancies(self):
-        self.control_adjancies = np.load(self.control_filepath, allow_pickle=True)
-        self.disease_adjancies = np.load(self.disease_filepath, allow_pickle=True)
-        return self.control_adjancies, self.disease_adjancies
+        self.possible_states_adjacency = {
+            "control": self.control_adjacency,
+            "disease": self.disease_adjacency,
+            "both": (self.control_adjacency, self.disease_adjacency)
+        }
 
-    def check_matrices_shape(self):
-        pass
+        self.filepaths_states = {"control": self.control_filepath, "disease": self.disease_filepath}
 
-    def make_grn_sergio(self):
+    def open_adjacency(self, open_both=False, which_state_to_open=""):
+        if open_both:
+            self.control_adjacency = np.load(self.control_filepath, allow_pickle=True)
+            self.disease_adjacency = np.load(self.disease_filepath, allow_pickle=True)
+            return self.control_adjacency, self.disease_adjacency
+        else:
+            if not open_both and which_state_to_open in self.possible_states_adjacency.keys():
+                self.possible_states_adjacency[which_state_to_open] = np.load(
+                    self.filepaths_states[which_state_to_open], allow_pickle=True
+                )
+                return self.possible_states_adjacency[which_state_to_open]
 
-        if self.select_percent_adjancies != 100:
+    def make_grn_sergio(self, make_one_state_only=False, make_which_state="control"):
+        if make_one_state_only and make_which_state in self.possible_states_adjacency.keys():
+            self.possible_states_adjacency[make_which_state] = self.open_adjacency(open_both=False,
+                                                                                   which_state_to_open=make_which_state)
+            self.make_one_state_grn(self.possible_states_adjacency[make_which_state])
+        else:
+            self.control_adjacency, self.disease_adjacency = self.open_adjacency(open_both=True)
+            self.make_two_states_grn()
 
-            if self.select_percent_adjancies <= 0 or isinstance(self.select_percent_adjancies, float):
-                raise EnvironmentError("'select_percent_adjancies' should be a int between [1,99]")
+    def make_one_state_grn(self, adjacency):
+        if self.select_percent_adjacency is not None:
+            adjacency = self.trim_interactions(adjacency)
 
-            self.trim_interactions(by_control_adjancies=True)
-
-        tot_unique_ids_control, master_regulons_control = self.unique_ids_and_master_regulons(self.control_adjancies)
-        tot_unique_ids_d, master_regulons_d = self.unique_ids_and_master_regulons(self.disease_adjancies)
-
-        genes_names_to_ids_mapping = {k: v for v, k in enumerate(tot_unique_ids_control)}
-        self.save_gene_ids_to_their_name_dict(genes_names_to_ids_mapping, "./scenicsergio/genes_ids_mapping.json")
-
-        self.interactions_tfs_per_target_gene = {}
-        visited_regulons = {k: False for k in tot_unique_ids_control}
-        with open(self.filepath_to_save_regulons, "w") as file_regulons:
-            for interaction in self.control_adjancies:
-                tf, target_gene, importance_c = interaction
-
-                match_in_disease = (self.disease_adjancies[:, 0] == tf) & (self.disease_adjancies[:, 1] == target_gene)
-                found_interaction_in_disease_too, = np.where(match_in_disease)
-
-                if tf in master_regulons_control and tf in master_regulons_d and not visited_regulons[tf]:
-                    # if found_interaction_in_disease_too:
-                    _, _, importance_d = self.disease_adjancies[found_interaction_in_disease_too].squeeze()
-                    file_regulons.write(f"{float(genes_names_to_ids_mapping[tf])},{importance_c},{importance_d}\n")
-                    visited_regulons[tf] = True
-                    break
-
-                if tf not in master_regulons_control and tf not in master_regulons_d:
-                    if found_interaction_in_disease_too:
-                        _, _, importance_d = self.disease_adjancies[found_interaction_in_disease_too].squeeze()
-
-                        avg_importance = (importance_c + importance_d) / 2
-                        self.add_interaction(genes_names_to_ids_mapping, avg_importance, target_gene, tf)
-                    else:
-                        self.add_interaction(genes_names_to_ids_mapping, importance_c, target_gene, tf)
-                else:
-                    logging.critical("Skipping the case when on gene is master in ony state only.")
+        cycle_edges_tobe_removed = self._break_cycles(adjacency)
+        tot_unique_ids, regulons, non_regulons_pairs = self.unique_ids_master_regulons_non_regulons_pairs(adjacency)
+        # genes_ids_to_names_mapping = {k: v for v, k in enumerate(tot_unique_ids)}
+        # self.save_gene_ids_to_their_name_dict(genes_ids_to_names_mapping)
+        # interactions_tfs_per_target_gene = self.group_interactions(adjacency, genes_ids_to_names_mapping)
 
         print("Done formatting pySCENIC GRN to SERGIO format.")
 
-    def add_interaction(self, genes_names_to_ids_mapping, importance, target, tf):
+    def group_interactions(self, adjacency, genes_names_to_ids_mapping):
+        """
+        Each tf is added to the bounded gene in the dictionary.
+        Example: {gene: ([tfs],[importance_values])}.
+        Each interaction is a tuple of (tf, target_gene, importance).
+        """
+        interactions_tfs_per_target_gene = {}
+        for interaction in adjacency:
+            tf, target_gene, importance = interaction
+            interactions_tfs_per_target_gene = self.add_interaction(
+                interactions_tfs_per_target_gene, genes_names_to_ids_mapping, interaction
+            )
+
+    def add_interaction(self, interactions_tfs_per_target_gene, genes_names_to_ids_mapping, interaction):
+        tf, target_gene, importance = interaction
         encoded_tf = genes_names_to_ids_mapping[tf]
-        encoded_target_gene = genes_names_to_ids_mapping[target]
-        if encoded_target_gene not in self.interactions_tfs_per_target_gene:
-            self.interactions_tfs_per_target_gene[encoded_target_gene] = ([encoded_tf], [importance])
+        encoded_target_gene = genes_names_to_ids_mapping[target_gene]
+        if encoded_target_gene not in interactions_tfs_per_target_gene:
+            interactions_tfs_per_target_gene[encoded_target_gene] = ([encoded_tf], [importance])
         else:
-            self.interactions_tfs_per_target_gene[encoded_target_gene][0].append(encoded_tf)
-            self.interactions_tfs_per_target_gene[encoded_target_gene][1].append(importance)
+            interactions_tfs_per_target_gene[encoded_target_gene][0].append(encoded_tf)
+            interactions_tfs_per_target_gene[encoded_target_gene][1].append(importance)
+        return interactions_tfs_per_target_gene
 
-    def trim_interactions(self, by_control_adjancies=True):
-        """Take only the target-TF interaction above this threshold. The original adjancies matrix is huge
+    def trim_interactions(self, adjacency):
+        """Take only the target-TF interaction above this threshold. The original adjacency matrix is huge
         and this simplifies the initial implementation"""
-        if by_control_adjancies:
-            select_until_row_x = int(len(self.control_adjancies) * (self.select_percent_adjancies / 100))
-            self.control_adjancies = self.control_adjancies[:select_until_row_x]
-            # else:
-            select_until_row_x = int(len(self.disease_adjancies) * (self.select_percent_adjancies / 100))
-            self.disease_adjancies = self.disease_adjancies[:select_until_row_x]
-        logging.info(f"The trimmed adjacency has #{select_until_row_x} interactions.")
+        select_until_row_x = int(len(adjacency) * (self.select_percent_adjacency / 100))
+        adjacency = adjacency[:select_until_row_x]
+        logging.info(f"The trimmed adjacency has now #{select_until_row_x} interactions.")
+        return adjacency
 
-    def unique_ids_and_master_regulons(self, adjancies):
-        tfs_set = np.unique(adjancies[:, 0].flatten())
-        target_genes_set = np.unique(adjancies[:, 1].flatten())
+    def unique_ids_master_regulons_non_regulons_pairs(self, adjacency):
+        tfs = adjacency[:, 0]
+        targets = adjacency[:, 1]
+        regulons = np.setdiff1d(tfs, targets)
+        non_regulons_pairs = [(tf, target) for tf, target in zip(tfs, targets) if tf not in regulons]
 
-        unique_master_regulons = set(tfs_set) - set(target_genes_set)
-        tot_ids_with_replicates = np.zeros(len(tfs_set) + len(target_genes_set), dtype=object)
-        tot_ids_with_replicates[:len(tfs_set)] = tfs_set
-        tot_ids_with_replicates[len(tfs_set):] = target_genes_set
-
+        tot_ids_with_replicates = np.zeros(len(tfs) + len(targets), dtype=object)
+        tot_ids_with_replicates[:len(tfs)] = tfs
+        tot_ids_with_replicates[len(tfs):] = targets
         tot_unique_ids = np.unique(tot_ids_with_replicates)
-
         print(f"Found unique TFs/genes {len(tot_unique_ids)}.")
-        return tot_unique_ids, unique_master_regulons
 
-    def save_gene_ids_to_their_name_dict(self, genes_ids_to_their_names_dict, filepath_to_save):
-        with open(f"{filepath_to_save}", "w") as file:
+        return tot_unique_ids, regulons, non_regulons_pairs
+
+    def save_gene_ids_to_their_name_dict(self, genes_ids_to_their_names_dict):
+        with open(f"{self.filepath_save_gene_name_to_id_mapping}", "w") as file:
             json.dump(genes_ids_to_their_names_dict, file)
+
+    def draw_graph(self, regulons_list, non_regulons_pairs, edges) -> []:
+        """Based on https://stackoverflow.com/questions/20133479/how-to-draw-directed-graphs-using-networkx-in-python"""
+        G = nx.DiGraph()
+        G.add_edges_from(edges)
+
+        val_map = {master_regulon: np.random.rand() for master_regulon in regulons_list}
+        values = [val_map.get(node, 0.2) for node in G.nodes()]
+
+        regulons_edges_red_edges = [edge for edge in G.edges() if edge not in non_regulons_pairs]
+
+        plt.figure(1, figsize=(12, 12))
+        pos = nx.spring_layout(G)
+        nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'), node_color=values, node_size=500)
+        nx.draw_networkx_labels(G, pos)
+        nx.draw_networkx_edges(G, pos, edgelist=regulons_edges_red_edges, edge_color='r', arrows=True)
+        nx.draw_networkx_edges(G, pos, edgelist=non_regulons_pairs, arrows=True)
+        plt.title("Gene Regulatory network - non master regulons have the same node color")
+
+        plt.show()
+
+        try:
+            cycle_edge = nx.find_cycle(G)
+            print(cycle_edge)
+            return cycle_edge
+        except nx.exception.NetworkXNoCycle:
+            return []
+
+    @staticmethod
+    def check_if_graph_is_cycled(edges) -> []:
+        G = nx.DiGraph()
+        G.add_edges_from(edges)
+        try:
+            cycle_edge = nx.find_cycle(G)
+            return cycle_edge
+        except nx.exception.NetworkXNoCycle:
+            return []
+
+    def _break_cycles(self, adjacency):
+        break_cycles = False
+        cycle_edges = []
+        edges = [(i[0], i[1]) for i in adjacency]
+        while not break_cycles:
+            cycle_edges_ = self.check_if_graph_is_cycled(edges)
+            cycle_edges += cycle_edges_
+            edges = [(i[0], i[1]) for i in adjacency if (i[0], i[1]) not in cycle_edges]
+            if not cycle_edges_:
+                break_cycles = True
+        return cycle_edges
 
     def format_sergio(self, adjacency, regulons_list, genes_names_to_indices):
         interactions_tfs_per_target_gene = {}
@@ -146,3 +197,42 @@ class ScenicSergioFlow(object):
                 importance_values_to_str = ",".join([str(importance) for importance in importances])
                 file.write(f"{float(target_gene_index)},{float(len(tfs))},{tf_for_target_gene_ids},"
                            f"{importance_values_to_str}\n")
+
+    def make_two_states_grn(self):
+        if self.select_percent_adjacency != 100:
+
+            if self.select_percent_adjacency <= 0 or isinstance(self.select_percent_adjacency, float):
+                raise EnvironmentError("'select_percent_adjacency' should be a int between [1,99]")
+
+            self.trim_interactions(by_control_adjacency=True)
+        tot_unique_ids_control, master_regulons_control = self.unique_ids_and_master_regulons(self.control_adjacency)
+        tot_unique_ids_d, master_regulons_d = self.unique_ids_and_master_regulons(self.disease_adjacency)
+        genes_names_to_ids_mapping = {k: v for v, k in enumerate(tot_unique_ids_control)}
+        self.save_gene_ids_to_their_name_dict(genes_names_to_ids_mapping, "./scenicsergio/genes_ids_mapping.json")
+        self.interactions_tfs_per_target_gene = {}
+        visited_regulons = {k: False for k in tot_unique_ids_control}
+        with open(self.filepath_to_save_regulons, "w") as file_regulons:
+            for interaction in self.control_adjacency:
+                tf, target_gene, importance_c = interaction
+
+                match_in_disease = (self.disease_adjacency[:, 0] == tf) & (self.disease_adjacency[:, 1] == target_gene)
+                found_interaction_in_disease_too, = np.where(match_in_disease)
+
+                if tf in master_regulons_control and tf in master_regulons_d and not visited_regulons[tf]:
+                    # if found_interaction_in_disease_too:
+                    _, _, importance_d = self.disease_adjacency[found_interaction_in_disease_too].squeeze()
+                    file_regulons.write(f"{float(genes_names_to_ids_mapping[tf])},{importance_c},{importance_d}\n")
+                    visited_regulons[tf] = True
+                    break
+
+                if tf not in master_regulons_control and tf not in master_regulons_d:
+                    if found_interaction_in_disease_too:
+                        _, _, importance_d = self.disease_adjacency[found_interaction_in_disease_too].squeeze()
+
+                        avg_importance = (importance_c + importance_d) / 2
+                        self.add_interaction(genes_names_to_ids_mapping, avg_importance, target_gene, tf)
+                    else:
+                        self.add_interaction(genes_names_to_ids_mapping, importance_c, target_gene, tf)
+                else:
+                    logging.critical("Skipping the case when on gene is master in ony state only.")
+        print("Done formatting pySCENIC GRN to SERGIO format.")
