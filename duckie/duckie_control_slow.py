99f221cfbaff019131684b45a22713529849500b
ioneliabuzatu <buzatuionelia@gmail.com>
2021-08-11 15:26:42 +0200

    expert in duckie_control_slow

--- duckie/duckie_control_slow.py
+++ duckie/duckie_control_slow.py
@@ -1,13 +1,16 @@
 import time
 
 import jax
-import jax.numpy as np
+import jax.numpy as jnp
+import numpy as onp
 
 from duckie import Ducky
+from classfier_cell_state import CellStateClassifier
 
 
 def main_control_steady_state():
     start = time.time()
+    expert = CellStateClassifier(num_genes=100).cuda()
     env = Ducky(
         number_genes=100,  # Features
         number_gene_types=2,  # Number of cell types
@@ -23,9 +26,9 @@ def main_control_steady_state():
 
     def loss_fn(actions):
         expr = env.simulate(actions)
-        return -np.mean(np.sum(np.power(expr, 2), axis=1))
+        return -jnp.mean(jnp.sum(jnp.power(expr, 2), axis=1))
 
-    actions = np.zeros((env.sampling_state_ * env.num_cells, env.num_cell_types, env.num_genes))
+    actions = jnp.zeros((env.sampling_state_ * env.num_cells, env.num_cell_types, env.num_genes))
     loss, grad = jax.value_and_grad(loss_fn)(actions)
     print("loss", loss)
     print(f"grad shape: {grad.shape} \n grad: {grad}")
